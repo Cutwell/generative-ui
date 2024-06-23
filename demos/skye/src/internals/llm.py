84d756e860fcc7714623594ecacc6bba1d6a3948
Zachary <22395357+Cutwell@users.noreply.github.com>
2024-06-23 17:17:48 +0100

    Updated Skye demo

--- demos/skye/src/internals/llm.py
+++ demos/skye/src/internals/llm.py
@@ -1,8 +1,9 @@
 import ollama
+import openai
+import json
 
 SYSTEM_PROMPT = """\
 You are a friendly, inquisitive, mental health chat assistant named Skye.
-You are a chat bot for the app Pacifica.
 Guide the user to answer a simple questionnaire to find out how they are feeling.
 Use a mixture of ice-breaker questions and questions about their mental health.
 Respond using html in two parts - first a reply to the previous input, second a html form component to get information from the user.
@@ -22,6 +23,7 @@ NEVER add extra assistant messages after user inputs in a single response.
 For multiple choice radio or checkbox questions, separate inputs with <br> tags to force new lines.
 Add <br> break tags between all labels and inputs to ensure they display properly.
 Only use ONE label for checkboxes or radio inputs to summarise the question, don't label every option (let the option itself be the label).
+Add the "required" attribute to inputs, unless that input is optional.
 
 Try to only ask a maximum of 2 questions at once.
 
@@ -40,7 +42,7 @@ Reply: ```html
 <div class="user">
     <label for="age">What is your name?</label>
     <br>
-    <input type="number" name="age">
+    <input type="number" name="age" required>
 </div>
 ```
 
@@ -60,49 +62,115 @@ Example components:
 """
 
 
-class DynamicUIChat:
-    def __init__(self):
+class ChatUser:
+    def __init__(self, guid):
+        self.guid = guid
+        self.model_name = "llama3"
+        self.openai_client = None
+        self.history = []
+
+        # Track profile of most recent form answers
+        self.profile = {}
+
         self.reset()
+
+    def chat(self, data):
+        # Get elements of data that are new, based on self.profile history
+        new_data = {
+            k: v
+            for k, v in data.items()
+            if k not in self.profile or self.profile[k] != v
+        }
+
+        # Update profile with the new data
+        self.profile = data
+
+        # Add the unique new form data to the chat history
+        self.history.append({"role": "user", "content": json.dumps(new_data, indent=4)})
+
+        # Step the chat by 1
+        response = self.call_llm()
+        self.history.append({"role": "assistant", "content": response})
+
+    def continue_chat(self):
+        # If chat exists and we're returning to it, renew the conversation
+        self.history.append(
+            {
+                "role": "user",
+                "content": f"All previous messages in this chat happened in a previous conversation.\nLet's continue our conversation.\nHere is a refresher of our last conversation:\n\n```json\n{json.dumps(self.profile, indent=4)}\n```\n\nReintroduce yourself, summarise the previous conversation and continue the chat.",
+            }
+        )
+
+        # Step the chat by 1
+        response = self.call_llm()
+        self.history.append({"role": "assistant", "content": response})
 
     def reset(self):
         self.history = [{"role": "system", "content": SYSTEM_PROMPT}]
+        # self.history.append({"role": "user", "content": str(content)})
+        response = self.call_llm()
+        self.history.append({"role": "assistant", "content": response})
 
-    def add_message(self, role, content):
-        self.history.append({"role": role, "content": str(content)})
-
-    def get_full_html(self):
-        # Provision to catch empty history
-        if len(self.history) <= 1:
-            self.next_state(form_contents={})
-
+    def get_all_html_blocks(self):
         html = []
+
+        # Collect all assistant messages (HTML blocks)
         for message in self.history:
             if message["role"] == "assistant":
-                content = message['content']
-                if (content.startswith("```html") and content.endswith("```")):
-                    content = content[7:-3]
+                content = message["content"]
+                content = self.strip_html_markdown_tags(markdown=content)
                 html.append(content)
-        
+
+        # Concatenate blocks
         html = "<br>".join(html)
 
         return html
 
-    def get_next_html(self):
-        content = self.history[-1]['content']
-        if (content.startswith("```html") and content.endswith("```")):
-            content = content[7:-3]
-        
+    def strip_html_markdown_tags(self, markdown):
+        if markdown.startswith("```html") and markdown.endswith("```"):
+            return markdown[7:-3]
+        else:
+            return markdown
+
+    def get_next_html_block(self):
+        content = self.history[-1]["content"]
+        content = self.strip_html_markdown_tags(markdown=content)
         return "<br>" + content
 
     def call_llm(self):
-        response = ollama.chat(
-            model="llama3",
-            messages=self.history,
-        )
-        return response["message"]["content"]
+        if self.model_name == "llama3":
+            response = ollama.chat(
+                model="llama3",
+                messages=self.history,
+            )
+            return response["message"]["content"]
+        elif self.model_name == "gpt-3.5-turbo":
+            chat_completion = self.openai_client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=self.history,
+            )
+            return chat_completion.choices[0].message.content
+        else:
+            raise Exception(f"Invalid model name: {self.model_name}")
 
-    def next_state(self, form_contents):
-        self.add_message(role="user", content=form_contents)
-        response = self.call_llm()
-        self.add_message(role="assistant", content=response)
-        return self.get_next_html()
+    def update_settings(self, model_name, openai_api_key):
+        if model_name == "gpt-3.5-turbo":
+            if check_openai_api_key(openai_api_key):
+                self.openai_client = openai.OpenAI(
+                    api_key=openai_api_key,
+                )
+            else:
+                return "Error: Invalid OpenAI API Key."
+
+        self.model_name = model_name
+        return "Success: Updated settings."
+
+
+def check_openai_api_key(api_key):
+    client = openai.OpenAI(api_key=api_key)
+    try:
+        client.models.list()
+    except openai.AuthenticationError:
+        return False
+    else:
+        return True
--- demos/skye/src/main.py
+++ demos/skye/src/main.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
 from contextlib import asynccontextmanager
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import os
+import uuid
 import pkg_resources
 
-from demos.skye.src.internals.llm import DynamicUIChat
+from demos.skye.src.internals.llm import ChatUser
+from demos.skye.src.internals.mem import MemCache
 
 
 @asynccontextmanager
@@ -35,7 +37,7 @@ async def lifespan(app: FastAPI):
         ],
     }
 
-    app.state.chat = DynamicUIChat()
+    app.state.cache = MemCache()
 
     yield  # wait for shutdown
 
@@ -59,25 +61,18 @@ if os.path.exists("static") and os.path.isdir("static"):
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get("/index", response_class=HTMLResponse)
+@app.exception_handler(404)
+async def not_found_exception_handler(request: Request, exc: HTTPException):
+    return templates.TemplateResponse("404.html", {"request": request}, status_code=404)
+
+
 @app.get("/", response_class=HTMLResponse)
-@app.post("/", response_class=HTMLResponse)
-@app.post("/index", response_class=HTMLResponse)
-async def get_html_page(
-    request: Request,
-):
-    """
-    Render current conversation as HTML form.
-
-    Returns:
-        html: Rendered index.html page, displaying the state the user as reached so far.
-    """
-
+@app.get("/index", response_class=HTMLResponse)
+def home(request: Request):
     return templates.TemplateResponse(
         "index.html",
         {
             "request": request,
-            "form": app.state.chat.get_full_html(),
             "page": app.state.page,
             "about": app.state.about,
             "project": app.state.project,
@@ -85,36 +80,119 @@ async def get_html_page(
     )
 
 
-@app.post("/submit", response_class=HTMLResponse)
-async def post_html_page(
-    request: Request
+@app.get("/new", response_class=RedirectResponse)
+def new_chat(
+    request: Request,
 ):
+    """
+    Create a new user chat object and redirect to unique chat page.
+
+    Returns:
+        RedirectResponse: redirect to a unique URL for the user's chat.
+    """
+
+    guid = str(uuid.uuid4())
+
+    new_user = ChatUser(guid=guid)
+
+    app.state.cache.set(key=guid, value=new_user)
+
+    return f"/chat/{guid}"
+
+
+@app.get("/chat/{user_uuid}", response_class=HTMLResponse)
+def load_chat(request: Request, user_uuid: str):
+    """
+    Load a chat from URL parameter UUID.
+
+    Returns:
+        html: Rendered index.html page, with chat history
+    """
+
+    user = app.state.cache.get(key=user_uuid)
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User UUID not found")
+    
+    if user.profile:
+        # Continue chat if profile exists
+        user.continue_chat()
+        html = user.get_next_html_block()
+    else:
+        # Else show entire conversation (implied new convo so only initial message)
+        html = user.get_all_html_blocks()
+
+    return templates.TemplateResponse(
+        "chat.html",
+        {
+            "request": request,
+            "form": html,
+            "page": app.state.page,
+            "about": app.state.about,
+            "project": app.state.project,
+        },
+    )
+
+
+@app.post("/chat/{user_uuid}", response_class=HTMLResponse)
+async def post_html_page(request: Request, user_uuid: str):
     """
     Update current conversation.
 
     Returns:
-        html: Rendered index.html page, displaying the state the user as reached so far.
+        JSONResponse: next HTML component for conversation.
     """
 
-    form = await request.json()
-    html = app.state.chat.next_state(form_contents=form)
+    data = await request.json()
+
+    user = app.state.cache.get(key=user_uuid)
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User UUID not found")
+
+    user.chat(data=data)
+
+    html = user.get_next_html_block()
 
     return JSONResponse(content={"content": html})
 
 
-@app.get("/reset", response_class=RedirectResponse)
-@app.post("/reset", response_class=RedirectResponse)
-async def reset(request: Request):
+@app.post("/settings/{user_uuid}", response_class=HTMLResponse)
+async def post_settings_page(request: Request, user_uuid: str):
     """
-    Reset chat state.
+    Update LLM.
 
     Returns:
-        html: Rendered index.html page, displaying the state the user as reached so far.
+        JSONResponse: action sucesss message.
     """
 
-    app.state.chat.reset()
+    data = await request.json()
 
-    return "/"
+    user = app.state.cache.get(key=user_uuid)
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User UUID not found")
+
+    res = user.update_settings(
+        model_name=data["model"], openai_api_key=data["api-token"]
+    )
+
+    return JSONResponse(content={"response": res})
+
+
+@app.get("/reset/{user_uuid}", response_class=RedirectResponse)
+@app.post("/reset/{user_uuid}", response_class=RedirectResponse)
+def reset(request: Request, user_uuid: str):
+    """
+    Delete chat history assigned to UUID.
+
+    Returns:
+        RedirectResponse: Redirect to index.html to establish new chat.
+    """
+
+    success = app.state.cache.delete(key=user_uuid)
+
+    return "/new"
 
 
 if __name__ == "__main__":
